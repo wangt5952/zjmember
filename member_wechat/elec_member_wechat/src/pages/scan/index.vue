12b8f523c6d6ecdadb77dc57af3b19aecda2e7aa
Galaxy_G <269274122@qq.com>
2018-06-14 17:49:55 +0800

    新增 扫码页面

--- member_wechat/elec_member_wechat/src/pages/scan/index.vue
+++ member_wechat/elec_member_wechat/src/pages/scan/index.vue
@@ -33,7 +33,6 @@ export default {
     });
     localStorage.setItem('redirect', 'scan')
     if (!this.member_id || this.member_id == null) {
-      debugger
       let wx_app_id = (await this.$http.get('/wx/appid')).data.wx_app_id //在线获取
       const redirectUri = `http://${location.hostname}/wx/code2openid`
       location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${wx_app_id}&redirect_uri=${redirectUri}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`
@@ -48,11 +47,11 @@ export default {
           MessageBox('兑换成功', '');
             this.$router.push('/memberLevel');
         } else if (data.data = -1) {
+          this.$router.push('/member');
           MessageBox('兑换失败', '失败原因：已兑换！');
-            this.$router.push('/member');
-        }else  if (data.data = 0) {
+        }else if (data.data = 0) {
+          this.$router.push('/member');
           MessageBox('兑换失败', '失败原因：输入信息有错误！');
-              this.$router.push('/member');
         }
        Indicator.close();
       } else {
